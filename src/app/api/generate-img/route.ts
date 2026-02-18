d3d0f5159fe036780a413ac9330ca1057e2d0161
Devendra Bisht <mail.devendra.bisht@gmail.com>
2026-02-19 02:37:44 +0530

    black-forest-labs/flux-2-pro image generation model added

--- src/app/api/generate-img/route.ts
+++ src/app/api/generate-img/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request) {
     };
     
     const replicate = new Replicate({ auth: process.env.REPLICATE_API_TOKEN });
-    const output: ReadableStream<readable> = await replicate.run(model, { input });
+    const output = await replicate.run(model, { input });
     // output is ReadableStream
     // ReadableStream { locked: false, state: 'readable', supportsBYOB: false }
     // URL {
@@ -90,10 +90,10 @@ export async function POST(req: Request) {
     const imgUrl = `${tempDirName}/${imgName}`;
     await writeFile(imgPath, output); // To write the file to disk
     
-    const imageUrl = output.url().href; // To access the file URL
+    // const imageUrl = output.url().href; // To access the file URL
     
     return Response.json({
-        image: imageUrl, // Passing image URL of generated image to frontend to be used as `src` of `img` tag
+        image: imgUrl, // Passing image URL of generated image to frontend to be used as `src` of `img` tag
         imgUrl: imgUrl, // Passing image URL of generated image to frontend to be used as `src` of `img` tag
         output: output, // Passing image URL of generated image to frontend to be used as `src` of `img` tag
     });
